910ddcd9acde05d32b67b29165eee0f67884ee7e
Alexx108 <alexx1088@gmail.com>
2022-10-25 21:23:38 +0600

    add update and delete methods

---
+++ resources/views/product/edit.blade.php
@@ -0,0 +1,92 @@
+
+@extends('layouts.main')
+
+@section('content')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Редактировать продукт</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+
+                        <li class="breadcrumb-item active">Главная</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <form action="{{route('product.update', $product->id)}}" method="post">
+                    @csrf
+                    @method('patch')
+                    <div class="form-group">
+                        <input type="text" name="title" value="{{ $product->title }}" class="form-control" placeholder="Наименование">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="description" value="{{ $product->description }}" class="form-control" placeholder="Описание">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="content" value="{{ $product->content }}" class="form-control" placeholder="Контент">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="price" class="form-control" value="{{ $product->price }}" placeholder="Цена">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="count" class="form-control" value="{{ $product->count }}" placeholder="Количество на складе">
+                    </div>
+
+                    <div class="form-group">
+                        <div class="input-group">
+                            <div class="custom-file">
+                                <input name="preview_image" type="file" value="{{ $product->preview_image }}" class="custom-file-input" id="exampleInputFile">
+                                <label class="custom-file-label" for="exampleInputFile">Выберите файл</label>
+                            </div>
+
+                            <div class="form-group">
+                                <select name="category_id"  class="form-control select2" style="width: 100%;">
+                                    <option selected="selected" disabled>Выберите категорию</option>
+                                    @foreach($categories as $category)
+                                        <option value="{{$category->id}}">{{$category->title}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <select class="tags" name="tags[]"  multiple="multiple" data-placeholder="Выберите тэг"
+                                        style="width: 100%;">
+                                    @foreach($tags as $tag)
+                                        <option value="{{$tag->id}}">{{$tag->title}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <select class="colors" name="colors[]" multiple="multiple" data-placeholder="Выберите цвет"
+                                        style="width: 100%;">
+                                    @foreach($colors as $color)
+                                        <option value="{{$color->id}}">{{$color->title}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="input-group-append">
+                                <span class="input-group-text">Загрузка</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Редактировать">
+                    </div>
+                </form>
+            </div>
+
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+@endsection
